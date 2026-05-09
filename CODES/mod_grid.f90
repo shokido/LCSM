f1f8f74586e692500ba1a52561118a4aa616ae0f
skido <skido@jamstec.go.jp>
2026-05-10 00:19:12 +0900

    Change set_mask routine

--- CODES/mod_grid.f90
+++ CODES/mod_grid.f90
@@ -3,36 +3,13 @@ module mod_grid
   use input_files
   implicit none
   private
-  public :: gen_cgrid
-  public :: create_mask
+  public :: set_mask
   public :: read_ocn_grid
-  public :: fname_in_grid,grid
-  character(maxlen) :: fname_in_grid
-  namelist/grid/fname_in_grid
 contains
-    !=============================================
-  ! C-Grid generation
-  !=============================================
-  subroutine gen_cgrid(nx,ny,x_p,y_p,x_u,x_v,y_u,y_v)
-    implicit none
-    integer(idx),intent(in) :: nx,ny
-    real(idx),intent(in) :: x_p(0:nx+1),y_p(0:ny+1)
-    real(idx),intent(inout),allocatable :: x_u(:),x_v(:)
-    real(idx),intent(inout),allocatable :: y_u(:),y_v(:)
-    integer :: ix,iy
-    allocate(x_u(1:nx+1)) ;  allocate(y_u(0:ny+1))
-    allocate(x_v(0:nx+1)) ;  allocate(y_v(1:ny+1))
-    ! X-direction
-    x_v=x_p
-    x_u(1:nx+1) = 0.5_idx * (x_p(0:nx)+x_p(1:nx+1))
-    ! y
-    y_u=y_p
-    y_v(1:ny) = 0.5_idx * (y_p(0:ny)+y_p(1:ny+1))
-  end subroutine gen_cgrid
   !=============================================
   ! Create mask
   !=============================================
-  subroutine create_mask(nx,ny,mask_p,mask_u,mask_v,mask_phi_u,mask_phi_v,slip)
+  subroutine set_mask(nx,ny,mask_p,mask_u,mask_v,mask_phi_u,mask_phi_v,slip)
     implicit none
     integer,intent(in) :: nx,ny
     real(idx),intent(in) :: mask_p(0:nx+1,0:ny+1)
@@ -40,19 +17,21 @@ contains
     real(idx),intent(inout) :: mask_phi_u(1:nx+1,1:ny+1), mask_phi_v(1:nx+1,1:ny+1)
     real(idx),intent(in) :: slip
     integer :: ix,iy
+    ! slip_0 (dun/dx=0),
+    ! slip=2 (un=0-> dun/dx = (un - (-un))/dx)
     mask_u(:,:)=1.0_idx
     mask_v(:,:)=1.0_idx
     mask_phi_u(:,:)=1.0_idx
     mask_phi_v(:,:)=1.0_idx
-    iy = 1
-    do ix = 2,nx
+    iy = 0
+    do ix = 1,nx+1
        mask_u(ix,iy)=mask_p(ix-1,iy)*mask_p(ix,iy)
     end do
 
-    do iy = 2,ny
-       ix = 1
+    do iy = 1,ny+1
+       ix = 0
        mask_v(ix,iy)=mask_p(ix,iy-1)*mask_p(ix,iy)       
-       do ix = 2,nx
+       do ix = 1,nx+1
           mask_u(ix,iy)=mask_p(ix-1,iy)*mask_p(ix,iy)
           mask_v(ix,iy)=mask_p(ix,iy-1)*mask_p(ix,iy)
           if (mask_p(ix-1,iy-1) == 0.0_idx .and. mask_p(ix,iy-1)==0.0_idx) then
@@ -69,7 +48,7 @@ contains
           end if
        end do
     end do
-  end subroutine create_mask
+  end subroutine set_mask
   subroutine read_ocn_grid(fname,nx,ny,x_p,y_p,x_u,y_u,x_v,y_v,&
        & lon_p,lat_p,lon_u,lat_u,lon_v,lat_v,f,mask_p,&
        & damp_p,damp_u,damp_v)
