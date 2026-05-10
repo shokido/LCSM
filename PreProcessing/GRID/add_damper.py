a4f7978d34f89d08a93ca9763a19a840a3703170
skido <skido@jamstec.go.jp>
2026-05-10 14:22:32 +0900

    Change preprocessing of damper

--- PreProcessing/GRID/add_damper.py
+++ PreProcessing/GRID/add_damper.py
@@ -1,55 +1,52 @@
 import numpy as np
 import netCDF4 as ncdf
 import sys
-factor=1.0#/(60.0*60.0*24.0)
-v1=1.0
-v2=10.0
-thres=300.0*1000
+factor=1.0/(60.0*60.0*24.0)
 
-grd_name="eqpac_30"
-fname_grid="grid_"+grd_name+".nc"
+gname="eqpac_30"
+fname_grid="grid_"+gname+".nc"
 nc1=ncdf.Dataset(fname_grid,"r+")
-lon_p=nc1.variables["lon_p"][:]
-y_u=nc1.variables["y_u_2d"][:,:]
-y_v=nc1.variables["y_v_2d"][:,:]
-damp_u=np.copy(nc1.variables["damp_u"][:,:])
-damp_v=np.copy(nc1.variables["damp_v"][:,:])
+y_u=nc1.variables["y_u"][:]
+damp_u=nc1.variables["damp_u"]
+damp_v=nc1.variables["damp_v"]
+damp_p=nc1.variables["damp_p"]
 
-y_u=y_u[:,0];y_v=y_v[:,0]
-ny_u=len(y_u)
-ny_v=len(y_v)
-ndim_u=np.shape(damp_u);nx_u=ndim_u[1]
-ndim_v=np.shape(damp_v);nx_v=ndim_v[1]
-damp_func_u=y_u.copy();damp_func_u[:]=0
-damp_func_v=y_v.copy();damp_func_v[:]=0
+#print(y_u)
+ny=len(y_u)
+ndim=np.shape(damp_u)
+nx=ndim[1]
+damp_func=y_u.copy()
+damp_func[:]=0
 
-# Northen boundary
-ymax=np.max(y_u)
-y2=ymax;y1=ymax-thres
-a=y_u[(y_u<=y2) & (y_u>=y1)]
-damp_func_u[:]=v1
-damp_func_v[:]=v1
-damp_func_u[(y_u<=y2) & (y_u>=y1)]=v1+(v2-v1)*(a-y1)/(y2-y1)
-ymax=np.max(y_v)
-y2=ymax;y1=ymax-thres
-a=y_v[(y_v<=y2) & (y_v>=y1)]
-damp_func_v[(y_v<=y2) & (y_v>=y1)]=v1+(v2-v1)*(a-y1)/(y2-y1)
+# --- Southern boundary sponge layer ---
+# sy1: Boundary of the fully damped region
+# sy2: Point where damping starts to transition from 0 to 1
+sy1 = 150.0 * 1000.0
+sy2 = 300.0 * 1000.0
 
-# Southern boundary
-y2=thres;y1=np.min(y_u)
-a=y_u[(y_u<=y2) & (y_u>=y1)]
+# Set values for the southern edge
+damp_func[y_u <= sy1] = 1.0
+s_mask = (y_u > sy1) & (y_u <= sy2)
+# Linear transition from 1.0 at sy1 to 0.0 at sy2
+damp_func[s_mask] = (sy2 - y_u[s_mask]) / (sy2 - sy1)
 
-damp_func_u[(y_u<=y2) & (y_u>=y1)]=v1+(v2-v1)*(y2-a)/(y2-y1)
-y2=thres;y1=np.min(y_v)
-a=y_v[(y_v<=y2) & (y_v>=y1)]
-damp_func_v[(y_v<=y2) & (y_v>=y1)]=v1+(v2-v1)*(y2-a)/(y2-y1)
+# --- Northern boundary sponge layer ---
+# Calculate based on the maximum Y coordinate
+y_max = np.max(y_u)
+ny1 = y_max - 150.0 * 1000.0  # 150km from the north end (1.0 north of here)
+ny2 = y_max - 300.0 * 1000.0  # 300km from the north end (transition start)
 
-for ix in range(0,nx_u):
-    damp_u[0:ny_u,ix]=damp_func_u[0:ny_u]*factor
+# Set values for the northern edge
+damp_func[y_u >= ny1] = 1.0
+n_mask = (y_u < ny1) & (y_u >= ny2)
+# Linear transition from 0.0 at ny2 to 1.0 at ny1
+damp_func[n_mask] = (y_u[n_mask] - ny2) / (ny1 - ny2)
 
-for ix in range(0,nx_v):
-    damp_v[0:ny_v,ix]=damp_func_v[0:ny_v]*factor
+# Map the 1D damping function to the 2D grid and apply the time factor
+for ix in range(0, nx):
+    damp_u[0:ny, ix] = damp_func[0:ny] * factor
 
-nc1.variables["damp_u"][:,:]=damp_u    
-nc1.variables["damp_v"][:,:]=damp_v
+damp_p[:]=0.0
+damp_v[:]=0.0
+# Save and close the file
 nc1.close()
