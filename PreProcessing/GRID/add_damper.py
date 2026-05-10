b3d9505cba94f23a87e47a3e860effb0d866d197
skido <skido@jamstec.go.jp>
2026-05-10 13:38:47 +0900

    Make grid generation file consistent with ZCM

--- PreProcessing/GRID/add_damper.py
+++ PreProcessing/GRID/add_damper.py
@@ -1,38 +1,55 @@
 import numpy as np
 import netCDF4 as ncdf
 import sys
-factor=1.0/(60.0*60.0*24.0)
+factor=1.0#/(60.0*60.0*24.0)
+v1=1.0
+v2=10.0
+thres=300.0*1000
 
-gname="indian"
-gname="eqpac_30"
-fname_grid="grid_"+gname+".nc"
+grd_name="eqpac_30"
+fname_grid="grid_"+grd_name+".nc"
 nc1=ncdf.Dataset(fname_grid,"r+")
-y_u=nc1.variables["y_u"][:]
-damp_u=nc1.variables["damp_u"]
+lon_p=nc1.variables["lon_p"][:]
+y_u=nc1.variables["y_u_2d"][:,:]
+y_v=nc1.variables["y_v_2d"][:,:]
+damp_u=np.copy(nc1.variables["damp_u"][:,:])
+damp_v=np.copy(nc1.variables["damp_v"][:,:])
 
-#print(y_u)
-ny=len(y_u)
-ndim=np.shape(damp_u)
-nx=ndim[1]
-damp_func=y_u.copy()
-damp_func[:]=0
+y_u=y_u[:,0];y_v=y_v[:,0]
+ny_u=len(y_u)
+ny_v=len(y_v)
+ndim_u=np.shape(damp_u);nx_u=ndim_u[1]
+ndim_v=np.shape(damp_v);nx_v=ndim_v[1]
+damp_func_u=y_u.copy();damp_func_u[:]=0
+damp_func_v=y_v.copy();damp_func_v[:]=0
+
+# Northen boundary
+ymax=np.max(y_u)
+y2=ymax;y1=ymax-thres
+a=y_u[(y_u<=y2) & (y_u>=y1)]
+damp_func_u[:]=v1
+damp_func_v[:]=v1
+damp_func_u[(y_u<=y2) & (y_u>=y1)]=v1+(v2-v1)*(a-y1)/(y2-y1)
+ymax=np.max(y_v)
+y2=ymax;y1=ymax-thres
+a=y_v[(y_v<=y2) & (y_v>=y1)]
+damp_func_v[(y_v<=y2) & (y_v>=y1)]=v1+(v2-v1)*(a-y1)/(y2-y1)
 
 # Southern boundary
-y1=150.0*1000.0
-y2=300.0*1000.0
-damp_func[y_u<=y1]=1.0
+y2=thres;y1=np.min(y_u)
 a=y_u[(y_u<=y2) & (y_u>=y1)]
-damp_func[(y_u<=y2) & (y_u>=y1)]=(y2-a)/(y1)
 
-# # Meridional boundary
-# damp_func[y_u<=150.0*1000.0]=1.0
-# a=y_u[(y_u<=300.0*1000.0) & (y_u>=150.0*1000.0)]
-# damp_func[(y_u<=300.0*1000.0) & (y_u>=150.0*1000.0)]=(300.0*1000.0-a)/(150.0*1000.0)
-# y_max=np.max(y_u)
-# damp_func[y_u>=y_max-150.0*1000.0]=1.0
-# a=y_max-y_u[(y_u>=y_max-300.0*1000.0) & (y_u<=y_max-150.0*1000.0)]
-# damp_func[(y_u>=y_max-300.0*1000.0) & (y_u<=y_max-150.0*1000.0)]=(300.0*1000.0-a)/(150.0*1000.0)
+damp_func_u[(y_u<=y2) & (y_u>=y1)]=v1+(v2-v1)*(y2-a)/(y2-y1)
+y2=thres;y1=np.min(y_v)
+a=y_v[(y_v<=y2) & (y_v>=y1)]
+damp_func_v[(y_v<=y2) & (y_v>=y1)]=v1+(v2-v1)*(y2-a)/(y2-y1)
 
-for ix in range(0,nx):
-    damp_u[0:ny,ix]=damp_func[0:ny]*factor
+for ix in range(0,nx_u):
+    damp_u[0:ny_u,ix]=damp_func_u[0:ny_u]*factor
+
+for ix in range(0,nx_v):
+    damp_v[0:ny_v,ix]=damp_func_v[0:ny_v]*factor
+
+nc1.variables["damp_u"][:,:]=damp_u    
+nc1.variables["damp_v"][:,:]=damp_v
 nc1.close()
--- PreProcessing/GRID/define_grid.py
+++ PreProcessing/GRID/define_grid.py
@@ -1,18 +1,15 @@
-# Python script for defining grid
+# python script for defining grid
 import numpy as np
-import netCDF4 as netCDF
+import netCDF4 as ncdf
 import math
 import os
 import subprocess
-
-fname_grid="grid_indian.nc"
-fname_gdes="gdes_grid_indian.dat"
-lon_w=28.0;lon_e=114.0;lat_s=-25.0;lat_n=25.0;dlon=1.0;dlat=1.0
 gname="eqpac_30"
-lon_w=110.0;lon_e=300.0;lat_s=-30.0;lat_n=30.0;dlon=2.5;dlat=1.0
+lon_w=110.0;lon_e=300.0;lat_s=-30.0;lat_n=30.0
+dlon=2.5;dlat=1.0
 fname_grid="grid_"+gname+".nc"
-fname_gdes="gdes_grid_"+gname+".dat"
-missing_value=9999.0
+fname_gdes="des_grid_"+gname+".dat"
+undef=-9999.9
 
 def cal_dis_lon_lat(lon1,lon2,lat1,lat2):
     R=6378137
@@ -40,15 +37,15 @@ def gen_cgrid(x_vert,y_vert):
     nx=x_vert.size ; ny=y_vert.size
     x1=1.5 * x_vert[0]-0.5 * x_vert[1]
     x2=1.5 * x_vert[nx-1] - 0.5 * x_vert[nx-2]
-    x_rho=np.linspace(x1,x2,nx+1)
+    x_p=np.linspace(x1,x2,nx+1)
     x_u=np.linspace(x_vert[0],x_vert[nx-1],nx)
     x_v=np.linspace(x1,x2,nx+1)
     y1=1.5 * y_vert[0]-0.5 * y_vert[1]
     y2=1.5 * y_vert[ny-1] - 0.5 * y_vert[ny-2]
-    y_rho=np.linspace(y1,y2,ny+1)
+    y_p=np.linspace(y1,y2,ny+1)
     y_u=np.linspace(y1,y2,ny+1)
-    y_v=np.linspace(y_vert[0],y_rho[ny-1],ny)
-    return(x_rho,y_rho,x_u,y_u,x_v,y_v)
+    y_v=np.linspace(y_vert[0],y_vert[ny-1],ny)
+    return(x_p,y_p,x_u,y_u,x_v,y_v)
 
     
 #========================================================
@@ -66,166 +63,233 @@ for ix in range(0,nx):
 for iy in range(0,ny):
     y_vert[iy]=cal_dis_lon_lat(lon_vert[0],lon_vert[0],lat_vert[0],lat_vert[iy])
 
-x_rho,y_rho,x_u,y_u,x_v,y_v=gen_cgrid(x_vert,y_vert)
-lon_rho,lat_rho,lon_u,lat_u,lon_v,lat_v=gen_cgrid(lon_vert,lat_vert)
+x_p,y_p,x_u,y_u,x_v,y_v=gen_cgrid(x_vert,y_vert)
+lon_p,lat_p,lon_u,lat_u,lon_v,lat_v=gen_cgrid(lon_vert,lat_vert)
 
-nx_rho=np.shape(x_rho)[0]
-ny_rho=np.shape(y_rho)[0]
+x_p_2d_out=np.zeros((len(y_p),len(x_p)));y_p_2d_out=np.zeros((len(y_p),len(x_p)))
+x_u_2d_out=np.zeros((len(y_u),len(x_u)));y_u_2d_out=np.zeros((len(y_u),len(x_u)))
+x_v_2d_out=np.zeros((len(y_v),len(x_v)));y_v_2d_out=np.zeros((len(y_v),len(x_v)))
+ix=0
+for iy in range(0,len(y_p)):
+    x_p_2d_out[iy,ix]=-1.0*cal_dis_lon_lat(lon_vert[0],lon_p[0],lat_p[iy],lat_p[iy])
+for iy in range(0,len(y_p)):
+    for ix in range(1,len(x_p)):
+        x_p_2d_out[iy,ix]=x_p_2d_out[iy,ix-1]+cal_dis_lon_lat(lon_p[ix-1],lon_p[ix],lat_p[iy],lat_p[iy])
+iy=0
+for ix in range(0,len(x_p)):
+    y_p_2d_out[iy,ix]=-1.0*cal_dis_lon_lat(lon_p[ix],lon_p[ix],lat_vert[0],lat_p[0])
+for iy in range(1,len(y_p)):
+    for ix in range(0,len(x_p)):
+        y_p_2d_out[iy,ix]=y_p_2d_out[iy-1,ix]+cal_dis_lon_lat(lon_p[ix],lon_p[ix],lat_p[iy-1],lat_p[iy])
+
+
+ix=0
+for iy in range(0,len(y_u)):
+    x_u_2d_out[iy,ix]=-1.0*cal_dis_lon_lat(lon_vert[0],lon_u[0],lat_u[iy],lat_u[iy])
+for iy in range(0,len(y_u)):
+    for ix in range(1,len(x_u)):
+        x_u_2d_out[iy,ix]=x_u_2d_out[iy,ix-1]+cal_dis_lon_lat(lon_u[ix-1],lon_u[ix],lat_u[iy],lat_u[iy])
+iy=0
+for ix in range(0,len(x_u)):
+    y_u_2d_out[iy,ix]=-1.0*cal_dis_lon_lat(lon_u[ix],lon_u[ix],lat_vert[0],lat_u[0])
+for iy in range(1,len(y_u)):
+    for ix in range(0,len(x_u)):
+        y_u_2d_out[iy,ix]=y_u_2d_out[iy-1,ix]+cal_dis_lon_lat(lon_u[ix],lon_u[ix],lat_u[iy-1],lat_u[iy])
+
+ix=0
+for iy in range(0,len(y_v)):
+    x_v_2d_out[iy,ix]=-1.0*cal_dis_lon_lat(lon_vert[0],lon_v[0],lat_v[iy],lat_v[iy])
+for iy in range(0,len(y_v)):
+    for ix in range(1,len(x_v)):
+        x_v_2d_out[iy,ix]=x_v_2d_out[iy,ix-1]+cal_dis_lon_lat(lon_v[ix-1],lon_v[ix],lat_v[iy],lat_v[iy])
+iy=0
+for ix in range(0,len(x_v)):
+    y_v_2d_out[iy,ix]=-1.0*cal_dis_lon_lat(lon_v[ix],lon_v[ix],lat_vert[0],lat_v[0])
+for iy in range(1,len(y_v)):
+    for ix in range(0,len(x_v)):
+        y_v_2d_out[iy,ix]=y_v_2d_out[iy-1,ix]+cal_dis_lon_lat(lon_v[ix],lon_v[ix],lat_v[iy-1],lat_v[iy])
+
+
+nx_p=np.shape(x_p)[0]
+ny_p=np.shape(y_p)[0]
 nx_u=np.shape(x_u)[0]
 ny_u=np.shape(y_u)[0]
 nx_v=np.shape(x_v)[0]
 ny_v=np.shape(y_v)[0]
 
 # Create Corioris parameter
-f=np.zeros(ny+1)
+f=np.zeros((ny+1,nx+1))
 for iy in range(0,ny+1):
-    f[iy]= cal_f(lat_rho[iy])
+    f[iy,:]= cal_f(lat_p[iy])
 
 # Create damper
-damp_p_out=np.zeros((ny_rho,nx_rho))
-damp_u_out=np.zeros((ny_u,nx_u))
-damp_v_out=np.zeros((ny_v,nx_v))
+damp_p_out=np.ones((ny_p,nx_p))
+damp_u_out=np.ones((ny_u,nx_u))
+damp_v_out=np.ones((ny_v,nx_v))
 
-mask_p=np.ones((ny_rho,nx_rho))
+mask_p=np.ones((ny_p,nx_p))
     
 isexist=os.path.exists(fname_grid)
 if (isexist):
     command=("rm",fname_grid)
     subprocess.check_call(command)
 
-nc_grid=netCDF.Dataset(fname_grid,"w")
-nc_grid.createDimension('x_p',nx_rho)
-nc_grid.createDimension('y_p',ny_rho)
+nc_grid=ncdf.Dataset(fname_grid,"w")
+nc_grid.createDimension('x_p',nx_p)
+nc_grid.createDimension('y_p',ny_p)
 nc_grid.createDimension('x_u',nx_u)
 nc_grid.createDimension('y_u',ny_u)
 nc_grid.createDimension('x_v',nx_v)
 nc_grid.createDimension('y_v',ny_v)
 
-xp = nc_grid.createVariable('x_p',x_rho.dtype, ('x_p'))
+xp = nc_grid.createVariable('x_p',x_p.dtype, ('x_p'))
 xp.long_name = 'zonal distance at p-grid'
 xp.units = 'm'
-xp.missing_value = 9999.
+xp.missing_value = undef
 
-yp = nc_grid.createVariable('y_p',y_rho.dtype, ('y_p'))
+yp = nc_grid.createVariable('y_p',y_p.dtype, ('y_p'))
 yp.long_name = 'zonal distance at p-grid'
 yp.units = 'm'
-yp.missing_value = 9999.
+yp.missing_value = undef
 
 xu = nc_grid.createVariable('x_u',x_u.dtype, ('x_u'))
 xu.long_name = 'zonal distance at u-grid'
 xu.units = 'm'
-xu.missing_value = 9999.
+xu.missing_value = undef
 
 yu = nc_grid.createVariable('y_u',y_u.dtype, ('y_u'))
 yu.long_name = 'zonal distance at u-grid'
 yu.units = 'm'
-yu.missing_value = 9999.
+yu.missing_value = undef
     
 xv = nc_grid.createVariable('x_v',x_v.dtype, ('x_v'))
 xv.long_name = 'zonal distance at v-grid'
 xv.vnits = 'm'
-xv.missing_valve = 9999.
+xv.missing_valve = undef
 
 yv = nc_grid.createVariable('y_v',y_v.dtype, ('y_v'))
 yv.long_name = 'zonal distance at v-grid'
 yv.vnits = 'm'
-yv.missing_valve = 9999.
+yv.missing_valve = undef
 
-lonp = nc_grid.createVariable('lon_p',x_rho.dtype, ('x_p'))
+lonp = nc_grid.createVariable('lon_p',x_p.dtype, ('x_p'))
 lonp.long_name = 'Longitude at p-grid'
 lonp.units = 'degrees_east'
-lonp.missing_value = 9999.
+lonp.missing_value = undef
 
-latp = nc_grid.createVariable('lat_p',y_rho.dtype, ('y_p'))
+latp = nc_grid.createVariable('lat_p',y_p.dtype, ('y_p'))
 latp.long_name = 'Latitude at p-grid'
 latp.units = 'degrees_north'
-latp.missing_value = 9999.
+latp.missing_value = undef
 
 lonu = nc_grid.createVariable('lon_u',x_u.dtype, ('x_u'))
 lonu.long_name = 'Longitude at u-grid'
 lonu.units = 'degrees_east'
-lonu.missing_value = 9999.
+lonu.missing_value = undef
 
 latu = nc_grid.createVariable('lat_u',y_u.dtype, ('y_u'))
 latu.long_name = 'Latitude at u-grid'
 latu.units = 'degrees_north'
-latu.missing_value = 9999.
+latu.missing_value = undef
 
 lonv = nc_grid.createVariable('lon_v',x_v.dtype, ('x_v'))
 lonv.long_name = 'Longitude at v-grid'
 lonv.units = 'degrees_east'
-lonv.missing_value = 9999.
+lonv.missing_value = undef
 
 latv = nc_grid.createVariable('lat_v',y_v.dtype, ('y_v'))
 latv.long_name = 'Latitude at v-grid'
 latv.units = 'degrees_north'
-latv.missing_value = 9999.
+latv.missing_value = undef
 
-fp = nc_grid.createVariable('f',y_rho.dtype, ('y_p'))
-fp.long_name = 'Coriolis parameter at p-grid'
-fp.units = '1/s'
-fp.missing_value = 9999.
-
-maskp = nc_grid.createVariable('mask_p',y_rho.dtype, ('y_p','x_p'))
+maskp = nc_grid.createVariable('mask_p',mask_p.dtype, ('y_p','x_p'))
 maskp.long_name = 'mask at p-grid'
 maskp.units = ''
-maskp.missing_value = 9999.
+maskp.missing_value = undef
 
-damp_p = nc_grid.createVariable('damp_p',y_rho.dtype, ('y_p','x_p'))
+masksst = nc_grid.createVariable('mask_sst',mask_p.dtype, ('y_p','x_p'))
+masksst.long_name = 'mask at sst-grid'
+masksst.units = ''
+masksst.missing_value = undef
+
+damp_p = nc_grid.createVariable('damp_p',y_p.dtype, ('y_p','x_p'))
 damp_p.long_name = 'damping coefficients at p-grid'
 damp_p.units = ''
-damp_p.missing_value = 9999.
+damp_p.missing_value = undef
 
-damp_u = nc_grid.createVariable('damp_u',y_rho.dtype, ('y_u','x_u'))
+damp_u = nc_grid.createVariable('damp_u',y_p.dtype, ('y_u','x_u'))
 damp_u.long_name = 'damping coefficients at u-grid'
 damp_u.units = ''
-damp_u.missing_value = 9999.
+damp_u.missing_value = undef
 
-damp_v = nc_grid.createVariable('damp_v',y_rho.dtype, ('y_v','x_v'))
+damp_v = nc_grid.createVariable('damp_v',y_p.dtype, ('y_v','x_v'))
 damp_v.long_name = 'damping coefficients at v-grid'
 damp_v.units = ''
-damp_v.missing_value = 9999.
+damp_v.missing_value = undef
+
+fp = nc_grid.createVariable('f',y_p.dtype, ('y_p','x_p'))
+fp.long_name = 'Coriolis parameter at p-grid'
+fp.units = '1/s'
+fp.missing_value = undef
+
+xp_2d = nc_grid.createVariable('x_p_2d',x_p.dtype, ('y_p','x_p'))
+xp_2d.long_name = '2d zonal distance at p-grid'
+xp_2d.units = 'm'
+xp_2d.missing_value = undef
+
+yp_2d = nc_grid.createVariable('y_p_2d',y_p.dtype, ('y_p','x_p'))
+yp_2d.long_name = '2d meridional distance at p-grid'
+yp_2d.units = 'm'
+yp_2d.missing_value = undef
+
+xu_2d = nc_grid.createVariable('x_u_2d',x_u.dtype, ('y_u','x_u'))
+xu_2d.long_name = '2d zonal distance at u-grid'
+xu_2d.units = 'm'
+xu_2d.missing_value = undef
+
+yu_2d = nc_grid.createVariable('y_u_2d',y_u.dtype, ('y_u','x_u'))
+yu_2d.long_name = '2d meridional distance at u-grid'
+yu_2d.units = 'm'
+yu_2d.missing_value = undef
+
+xv_2d = nc_grid.createVariable('x_v_2d',x_v.dtype, ('y_v','x_v'))
+xv_2d.long_name = '2d zonal distance at v-grid'
+xv_2d.units = 'm'
+xv_2d.missing_value = undef
+
+yv_2d = nc_grid.createVariable('y_v_2d',y_v.dtype, ('y_v','x_v'))
+yv_2d.long_name = '2d meridional distance at v-grid'
+yv_2d.units = 'm'
+yv_2d.missing_value = undef
     
-    
-xp[:]=x_rho
-yp[:]=y_rho
+xp[:]=x_p
+yp[:]=y_p
 xu[:]=x_u
 yu[:]=y_u
 xv[:]=x_v
 yv[:]=y_v
     
-lonp[:]=lon_rho
-latp[:]=lat_rho
+lonp[:]=lon_p
+latp[:]=lat_p
 lonu[:]=lon_u
 latu[:]=lat_u
 lonv[:]=lon_v
 latv[:]=lat_v
-fp[:]=f
+fp[:,:]=f
 maskp[:,:]=mask_p
+masksst[:,:]=mask_p
 damp_u[:,:]=damp_u_out
 damp_v[:,:]=damp_v_out
 damp_p[:,:]=damp_p_out
+xp_2d[:,:]=x_p_2d_out;yp_2d[:,:]=y_p_2d_out
+xu_2d[:,:]=x_u_2d_out;yu_2d[:,:]=y_u_2d_out
+xv_2d[:,:]=x_v_2d_out;yv_2d[:,:]=y_v_2d_out
 
 nc_grid.close()    
-
-# line='gridtype = lonlat'
-# print(line)
-# line='xsize    =  '+str(nx_rho)
-# print(line)
-# line='ysize    =  '+str(ny_rho)
-# print(line)
-# line='xfirst   = '+str(lon_rho[0])
-# print(line)
-# line='xinc     =  '+str(dlon)
-# print(line)
-# line='yfirst   = '+str(lat_rho[0])
-# print(line)
-# line='xinc     =  '+str(dlat)
-# print(line)
-xsize=np.shape(lon_rho)[0]
-ysize=np.shape(lat_rho)[0]
+xsize=np.shape(lon_p)[0]
+ysize=np.shape(lat_p)[0]
 gridsize=xsize*ysize
-lon_2d,lat_2d=np.meshgrid(lon_rho,lat_rho)
+lon_2d,lat_2d=np.meshgrid(lon_p,lat_p)
 lon_1d=np.reshape(lon_2d,[gridsize])
 lat_1d=np.reshape(lat_2d,[gridsize])
 
